7e7bd7e9c6bd86acb283a1f730a8ae68eac6cd8b
Chris Koster <chris@codecapi.nl>
2019-12-09 16:45:22 +0100

    set correct filename for feedretrieval

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,23 +1,23 @@
-var FeedRetrieval = require("./feed_retrieval");
+var FeedRetrieval = require("./feedretrieval");
 var FeedNode = require("./feed-node");
 
 var feedRetrieval = new FeedRetrieval();
 
-exports.sourceNodes = async ({ actions }, pluginOptions ) => {
-    const { createNode } = actions
-    
-    const category = pluginOptions.category;
-    const feeds = pluginOptions.feeds;
+exports.sourceNodes = async ({ actions }, pluginOptions) => {
+  const { createNode } = actions;
 
-    feeds.forEach(feed => {
-        feedRetrieval.request(feed).then(data => {
-            data.forEach(d => {
-                d.category = category;
-                const feedNode = FeedNode(d);
-                createNode(feedNode);
-            })
-        })
+  const category = pluginOptions.category;
+  const feeds = pluginOptions.feeds;
+
+  feeds.forEach(feed => {
+    feedRetrieval.request(feed).then(data => {
+      data.forEach(d => {
+        d.category = category;
+        const feedNode = FeedNode(d);
+        createNode(feedNode);
+      });
     });
+  });
 
-    return
-  }
+  return;
+};
